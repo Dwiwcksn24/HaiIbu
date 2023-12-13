410fee88a8917499ef6eb7aa98e5206373db0f30
Dwiwcksn24 <75288510+Dwiwcksn24@users.noreply.github.com>
2023-12-13 22:42:15 +0700

    Update

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 const messages = [
   "Hai Bu",
-  "Matahari tidak selalu menampakkan cahayanya",
-  "Tapi aku percaya",
-  "Ibu akan selalu menampakkan dirimu",
-  "Disaat aku membutuhkannya",
-  "Ibu Adalah Malaikatku",
-  "Aku sayang ibu ❤️",
+  "Matahari tidak selalu", 
+  "Menampakkan cahayanya",
+  "Pada setiap sorotan hari",
+  "Tapi aku percaya..",
+  "Ibu selalu menampakkan dirinya",
+  "Saat aku membutuhkanya",
+  "Ibu Kaulah Malaikatku",
+  "Terima Kasih Ibu",
+  "Aku Sayang Ibu ❤️"
   // Add more messages here
 ];
 
-const bgMusic = document.getElementById('bg-music');
+const music = document.getElementById('bg-music');
+const music_hide = document.getElementById('music_player');
 const messageElement = document.getElementById('message');
 const showVideoButton = document.getElementById('showVideo');
 const videoPlayer = document.getElementById('videoPlayer');
 const youtubePlayer = document.getElementById('youtubePlayer');
+const messageDiv = document.getElementById('message');
+const videoDiv = document.getElementById('video');
 
 let index = 0;
 
@@ -27,7 +33,21 @@ function displayMessage() {
   }
 }
 
-
+function showMessage() {
+  if (index < messages.length) {
+    messageDiv.innerText = messages[index];
+    messageDiv.style.display = 'block';
+    setTimeout(() => {
+      messageDiv.style.display = 'none';
+      index++;
+      showMessage();
+    }, 5000); // Show each message for 5 seconds
+  } else {
+    videoDiv.style.display = 'block';
+    music.pause();
+    music_hide.style.display = 'none';
+  }
+}
   function showVideoPlayer() {
   messageElement.style.display = 'none';
   videoPlayer.style.display = 'click';
@@ -35,6 +55,7 @@ function displayMessage() {
   youtubePlayer.src = 'https://www.youtube.com/embed/CwGbMYLjIpQ?si=KkUenjX-g__SLbxQ';
 }
 
+// showMessage();
 
 // Start displaying messages
-displayMessage();
+// displayMessage();
