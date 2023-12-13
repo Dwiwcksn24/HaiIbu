1a98dbff387eeb509f37e9e6b353ffba7c1fbb5a
Dwiwcksn24 <75288510+Dwiwcksn24@users.noreply.github.com>
2023-12-13 20:22:56 +0700

    Update

--- script.js
+++ script.js
@@ -9,22 +9,30 @@ const messages = [
   // Add more messages here
 ];
 
-const bgMusic = document.getElementById('bg-music');
+const music = document.getElementById('bg-music');
+const playPauseButton = document.getElementById('playPause');
 const messageElement = document.getElementById('message');
 const showVideoButton = document.getElementById('showVideo');
 const videoPlayer = document.getElementById('videoPlayer');
 const youtubePlayer = document.getElementById('youtubePlayer');
 
 let index = 0;
-
+playPauseButton.addEventListener('click', function() {
+  if (music.paused) {
+    music.play();
+    playPauseButton.textContent = 'Pause';
+  } else {
+    music.pause();
+    playPauseButton.textContent = 'Play';
+  }
+})
 function displayMessage() {
   if (index < messages.length) {
     messageElement.textContent = messages[index];
     index++;
     setTimeout(displayMessage, 3000); // Change the time here (in milliseconds)
-  } else showVideoButton.addEventListener('click', function() {{
-    showVideoPlayer= 'block';
-  }
+  } else showVideoButton.addEventListener('click', function() {
+    showVideoPlayer= 'block';})
 }
 
 function showVideoPlayer() {
