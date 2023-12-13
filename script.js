b3a83d331ced1099c0070910d343fe57cf5d8989
Dwiwcksn24 <75288510+Dwiwcksn24@users.noreply.github.com>
2023-12-13 18:27:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,8 +9,7 @@ const messages = [
   // Add more messages here
 ];
 
-const music = document.getElementById('bg-music');
-const playPauseButton = document.getElementById('playPause');
+const bgMusic = document.getElementById('bg-music');
 const messageElement = document.getElementById('message');
 const showVideoButton = document.getElementById('showVideo');
 const videoPlayer = document.getElementById('videoPlayer');
@@ -23,34 +22,10 @@ function displayMessage() {
     messageElement.textContent = messages[index];
     index++;
     setTimeout(displayMessage, 3000); // Change the time here (in milliseconds)
-  } else showVideoButton.addEventListener('click'), function() {
+  } else showVideoButton.addEventListener('click', function() {{
     showVideoPlayer= 'block';
   }
-// Function to check if an element is in the viewport
-function isElementInViewport(el) {
-  var rect = el.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
 }
-
-// Function to play or pause the background music based on video player visibility
-function controlBackgroundMusic() {
-  var videoPlayer = document.querySelector('.video-player');
-
- 
-playPauseButton.addEventListener('click', function() {
-  if (music.paused) {
-    music.play();
-    playPauseButton.textContent = 'Pause';
-  } else {
-    music.pause();
-    playPauseButton.textContent = 'Play';
-  }
-});
 
 function showVideoPlayer() {
   messageElement.style.display = 'none';
@@ -61,5 +36,3 @@ function showVideoPlayer() {
 
 // Start displaying messages
 displayMessage();
-}
-}
