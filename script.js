3b9e797dc738a2c7fb6a964c93d4c52bf0d34d85
Dwiwcksn24 <75288510+Dwiwcksn24@users.noreply.github.com>
2023-12-13 20:39:19 +0700

    Update

--- script.js
+++ script.js
@@ -9,33 +9,35 @@ const messages = [
   // Add more messages here
 ];
 
-const music = document.getElementById('bg-music');
-const playPauseButton = document.getElementById('playPause');
+const bgMusic = document.getElementById('bg-music');
 const messageElement = document.getElementById('message');
 const showVideoButton = document.getElementById('showVideo');
 const videoPlayer = document.getElementById('videoPlayer');
 const youtubePlayer = document.getElementById('youtubePlayer');
 
 let index = 0;
-playPauseButton.addEventListener('click', function() {
-  if (music.paused) {
-    music.play();
-    playPauseButton.textContent = 'Pause';
-  } else {
-    music.pause();
-    playPauseButton.textContent = 'Play';
-  }
-})
+
 function displayMessage() {
   if (index < messages.length) {
     messageElement.textContent = messages[index];
     index++;
     setTimeout(displayMessage, 3000); // Change the time here (in milliseconds)
-  } else showVideoButton.addEventListener('click', function() {
-    showVideoPlayer= 'block';})
+  } else showVideoButton.addEventListener('click'), function() {
+    showVideoPlayer= 'block';
+  }
 }
+youtubePlayer.addEventListener('play', () => {
+  bg-music.pause();
+});
+youtubePlayer.addEventListener('pause', () => {
+  bg-music.play();
+});
 
-function showVideoPlayer() {
+youtubePlayer.addEventListener('ended', () => {
+  bg-music.play();
+});
+
+  function showVideoPlayer() {
   messageElement.style.display = 'none';
   videoPlayer.style.display = 'click';
   // Replace 'VIDEO_ID' with the YouTube video ID you want to play
